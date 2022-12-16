27fa0b322962ad8823d85ef5a35ff3c648f6afab
sid23489 <siddharth234890@gmail.com>
2022-12-16 18:29:30 +0530

    INCREASE QTY FUNC

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,22 +1,41 @@
+import { getByTitle } from "@testing-library/react";
 import React from "react";
 class CartItem extends React.Component
 {
+    constructor()
+    {
+        super();
+        this.state={
+            price:9999,
+            title:'Phone',
+            qty:1,
+            img:''
+        }
+    }
+    increaseQuantity=()=>
+    {
+        console.log("TEST");
+    }
     render()
     {
+        const{price,title,qty}=this.state;
         return (
 <div className="cart-item">
     <div className="left-block">
      <img style={styles.image}/>
     </div>
     <div className="right-block">
-    <div style={{fontSize:25}}>PHONE</div>
-    <div style={{color:'green'}}>RS 9999</div>
-    <div style={{color:'blue'}}>QTY: 1</div>
+    <div style={{fontSize:25}}>{title}</div>
+    <div style={{color:'green'}}>{price}</div>
+    <div style={{color:'blue'}}>Qty:{qty}</div>
     <div className="cart-item-actions">
         {/*buttons*/}
+        <img alt ="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png"/>
+        <img alt ="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"  onClick ={this.increaseQuantity}/>
+        <img alt ="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" />
     </div>
     </div>
-</div>
+    </div>
         );
     }
 }
